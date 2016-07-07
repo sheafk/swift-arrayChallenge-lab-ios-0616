008adc2de9d6746be09bf00008a6bfc1155c9b90
sheafk <sheafureyking@gmail.com>
2016-07-07 14:08:09 -0400

    Completed the lab. -S

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
@@ -15,11 +16,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        let food: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
+        
+        let numbers: [Int] = [6, 4, 12, 4]
+        
+        shoppingList = makeShoppingList(food, quantityOfItems: numbers)
+        print(shoppingList)
         
         
     }
     
-    
+    func makeShoppingList(foods: [String], quantityOfItems:[Int]) -> [String] {
+        
+        var repairedList: [String] = []
+        
+        for (index, number) in quantityOfItems.enumerate() {
+            
+            let combinedItem = "\(index + 1). \(number) " + foods[index]
+            
+            repairedList.append(combinedItem)
+        }
+        
+        return repairedList
+        
+    }
     
 }
+
